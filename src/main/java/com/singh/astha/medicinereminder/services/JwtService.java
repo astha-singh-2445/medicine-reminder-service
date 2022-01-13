7f7bcf8c41599d239f5dc641f2a948cce0070bf3
Astha <asthasingh2445@gmail.com>
2022-01-13 20:39:11 +0530

    "wrong push " Revert "Security configuartion"
    This reverts commit c072894f7cca144ec3a8bde293e3f99e9632797e.

--- src/main/java/com/singh/astha/medicinereminder/services/JwtService.java
+++
@@ -1,9 +0,0 @@
-package com.singh.astha.medicinereminder.services;
-
-
-import com.singh.astha.medicinereminder.dtos.JwtPayload;
-
-public interface JwtService {
-
-    JwtPayload verifyAndDecodeToken(String authHeader);
-}
