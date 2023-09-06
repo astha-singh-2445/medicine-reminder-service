f42513ddc13dad5c024e73ef53cb32f19f48825d
asthasingh2445@gmail.com <asthasingh2445@gmail.com>
2023-09-06 18:11:03 +0530

    remove unused code

--- src/main/java/com/singh/astha/medicinereminder/controller/HealthController.java
+++ src/main/java/com/singh/astha/medicinereminder/controller/HealthController.java
@@ -1,8 +1,5 @@
 package com.singh.astha.medicinereminder.controller;
 
-import com.singh.astha.medicinereminder.producer.EventReminderProducer;
-import com.singh.astha.medicinereminder.repository.EventReminderRepository;
-import com.singh.astha.medicinereminder.repository.MedicineRepository;
 import com.singh.astha.medicinereminder.utils.Constants;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-
-    private final EventReminderRepository eventReminderRepository;
-    private final EventReminderProducer eventReminderProducer;
-    private final MedicineRepository medicineRepository;
-
-    public HealthController(EventReminderRepository eventReminderRepository,
-                            EventReminderProducer eventReminderProducer,
-                            MedicineRepository medicineRepository) {
-        this.eventReminderRepository = eventReminderRepository;
-        this.eventReminderProducer = eventReminderProducer;
-        this.medicineRepository = medicineRepository;
-    }
-
 
     @GetMapping(value = "/health")
     public ResponseEntity<String> getHealthApi() {
